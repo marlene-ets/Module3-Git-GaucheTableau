aa828c1e61f568c2b431f045ac3680429cf88f55
Ilona <ilona.daumas.1@ens.etsmtl.ca>
2025-05-14 22:23:25 +0200

    Ce programme demande si l'utilisateur préfère être tutoyé ou vouvoyé, selon la réponse la réponse du print initial est changé.

--- salutations.py
+++ salutations.py
@@ -1,8 +1,12 @@
 """Ce programme va nous servir à saluer une personne"""
 
 
-print('Bonjour à toi !')
-
+#Demande si l'utilisateur préfère être tutoyé ou vouvoyé
+preference = input('Entrez oui si vous préférez être vouvoyez : ')
+if preference == 'oui':
+    print('Bonjour à vous')
+elif preference == 'non' :
+    print('Bonjour à toi')
 nom = input("Quel est votre nom? ")
 
 print(nom)
