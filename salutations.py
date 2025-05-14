d6cb491b5a3bbc3d9b5792bbc4b560bf21a32d28
Lina <lina.seghilani.1@ens.etsmtl.ca>
2025-05-14 16:22:37 -0400

    demander lage

--- salutations.py
+++ salutations.py
@@ -1,4 +1,9 @@
 """Ce programme va nous servir à saluer une personne"""
-
+age=input("voulez entrez votre age ")
 
 print('Bonjour à toi !')
+print(f'Vous avez {age} ans')
+
+nom = input("Quel est votre nom? ")
+
+print(nom)
